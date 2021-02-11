a34303de78cd147fc9494ba629e630d3e2e3ba06
OskinDI <denis_oskin@rambler.ru>
2021-02-12 01:03:24 +0300

    Full code

---
+++ src/test/java/ru/geekbrains/oskindi/javacore2/Main.java
@@ -0,0 +1,35 @@
+package ru.geekbrains.oskindi.javacore2;
+
+public class Main {
+    public static void main(String[] args) {
+        Robot robot1 = new Robot("T-2000", 50000, 5);
+        Human human1 = new Human("Denis", 10000, 3);
+        Cat cat1 = new Cat("Leopold", 5000, 1);
+
+        Wall wall1 = new Wall(3);
+        Treadmill treadmill1 = new Treadmill(3000);
+        Treadmill treadmill2 = new Treadmill(15000);
+
+        Obstacle[] obstacles = {wall1, treadmill1, treadmill2};
+        Participant[] participants = {robot1, human1, cat1};
+
+        for (Participant character: participants) {
+            character.info();
+        }
+
+        for (int i = 0; i < participants.length; i++) {
+            for (int j = 0; j < obstacles.length; j++) {
+                if (participants[i].isOnDistance()) {
+                    obstacles[j].overcome(participants[i]);
+                } else {
+                    break;
+                }
+
+            }
+        }
+
+
+    }
+
+
+}
---
+++ src/test/java/ru/geekbrains/oskindi/javacore2/Obstacle.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.oskindi.javacore2;
+
+public abstract class Obstacle {
+
+    public abstract void overcome(Participant participant);
+
+
+}
